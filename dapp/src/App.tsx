d9990330ddf2ec6ce7eba0b5dd3e57e85bfa3fa7
Benjamin Fuentes <benjamin.fuentes@marigold.dev>
2022-03-10 14:17:11 +0100

    let s try like this :)

--- dapp/src/App.tsx
+++ dapp/src/App.tsx
@@ -1,51 +1,75 @@
 import { useState } from 'react';
 import './App.css';
 import ConnectButton from './ConnectWallet';
-import { TezosToolkit } from '@taquito/taquito';
+import { TezosToolkit, WalletContract } from '@taquito/taquito';
 import DisconnectButton from './DisconnectWallet';
+import { Contract, ContractsService } from '@dipdup/tzkt-api';
 
 function App() {
-
+  
   const [Tezos, setTezos] = useState<TezosToolkit>(new TezosToolkit("https://hangzhounet.tezos.marigold.dev"));
-  const [publicToken, setPublicToken] = useState<string | null>("");
   const [wallet, setWallet] = useState<any>(null);
   const [userAddress, setUserAddress] = useState<string>("");
   const [userBalance, setUserBalance] = useState<number>(0);
-  const [beaconConnection, setBeaconConnection] = useState<boolean>(false);
-
+  
+  //tzkt
+  const contractsService = new ContractsService( {baseUrl: "https://api.hangzhounet.tzkt.io" , version : "", withCredentials : false});
+  const [contracts, setContracts] = useState<Array<Contract>>([]);
+  
+  const fetchContracts = () => {
+    (async () => {
+      setContracts((await contractsService.getSimilar({address:"KT1M1sXXUYdLvow9J4tYcDDrYa6aKn3k1NT9" , includeStorage:true, sort:{desc:"id"}})));
+    })();
+  }
+  
+  //poke
+  const poke = async (contract : Contract) => {   
+    let c : WalletContract = await Tezos.wallet.at(""+contract.address);
+    try {
+      const op = await c.methods.default().send();
+      await op.confirmation();
+    } catch (error : any) {
+      console.log(error);
+      console.table(`Error: ${JSON.stringify(error, null, 2)}`);
+    }
+  };
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-        
-        <ConnectButton
-          Tezos={Tezos}
-          setPublicToken={setPublicToken}
-          setWallet={setWallet}
-          setUserAddress={setUserAddress}
-          setUserBalance={setUserBalance}
-          setBeaconConnection={setBeaconConnection}
-          wallet={wallet}
-        />
-        
-        <DisconnectButton
-          wallet={wallet}
-          setPublicToken={setPublicToken}
-          setUserAddress={setUserAddress}
-          setUserBalance={setUserBalance}
-          setWallet={setWallet}
-          setBeaconConnection={setBeaconConnection}
-        />
-
-        <div>
-        I am {userAddress} with {userBalance} Tz
-        </div>
-
-        </p>
-
-      </header>
+    <header className="App-header">
+    <p>
+    
+    <ConnectButton
+    Tezos={Tezos}
+    setWallet={setWallet}
+    setUserAddress={setUserAddress}
+    setUserBalance={setUserBalance}
+    wallet={wallet}
+    />
+    
+    <DisconnectButton
+    wallet={wallet}
+    setUserAddress={setUserAddress}
+    setUserBalance={setUserBalance}
+    setWallet={setWallet}
+    />
+    
+    <div>
+    I am {userAddress} with {userBalance} Tz
     </div>
-  );
-}
-
-export default App;
+    
+    <br />
+    <div>
+    <button onClick={fetchContracts}>Fetch contracts</button>
+    {contracts.map((contract) => <div>{contract.address} (poke guyz:{contract.storage.join(",")})<button onClick={() =>poke(contract)}>Poke</button></div>)}
+    </div>
+    </p>
+    
+    </header>
+    </div>
+    );
+  }
+  
+  export default App;
+  
--- dapp/src/DisconnectWallet.tsx
+++ dapp/src/DisconnectWallet.tsx
@@ -3,27 +3,21 @@ import { BeaconWallet } from "@taquito/beacon-wallet";
 
 interface ButtonProps {
   wallet: BeaconWallet | null;
-  setPublicToken: Dispatch<SetStateAction<string | null>>;
   setUserAddress: Dispatch<SetStateAction<string>>;
   setUserBalance: Dispatch<SetStateAction<number>>;
   setWallet: Dispatch<SetStateAction<any>>;
-  setBeaconConnection: Dispatch<SetStateAction<boolean>>;
 }
 
 const DisconnectButton = ({
   wallet,
-  setPublicToken,
   setUserAddress,
   setUserBalance,
   setWallet,
-  setBeaconConnection
 }: ButtonProps): JSX.Element => {
   const disconnectWallet = async (): Promise<void> => {
     setUserAddress("");
     setUserBalance(0);
     setWallet(null);
-    setBeaconConnection(false);
-    setPublicToken(null);
     console.log("disconnecting wallet");
     if (wallet) {
       await wallet.client.removeAllAccounts();
