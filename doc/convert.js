af1dfe47064bc9968e3a6624f12a6146d03486a7
Benjamin Fuentes <benjamin.fuentes@marigold.dev>
2022-12-20 16:14:32 +0100

    doc for www

---
+++ doc/convert.js
@@ -0,0 +1,12 @@
+const MarkdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
+const markdownHSections = require("markdown-it-header-sections");
+const fs = require("fs");
+const MD = new MarkdownIt({ html: true });
+MD.use(markdownItAttrs).use(markdownHSections);
+const md = fs.readFileSync("../README.md", { encoding: "utf8" });
+const body = MD.render(md);
+
+const template = fs.readFileSync("./template.html", { encoding: "utf8" });
+const html = template.replace("{body}", body);
+fs.writeFileSync("./www/index.html", html);
