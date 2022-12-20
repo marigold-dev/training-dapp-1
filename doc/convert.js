1bed1912b046d7e7c93f11b057fe6dd33f6a252f
Benjamin Fuentes <benjamin.fuentes@marigold.dev>
2022-12-20 17:19:45 +0100

    highlighter

--- doc/convert.js
+++ doc/convert.js
@@ -2,9 +2,22 @@ const MarkdownIt = require("markdown-it");
 const markdownItAttrs = require("markdown-it-attrs");
 const markdownHSections = require("markdown-it-header-sections");
 const emoji = require("markdown-it-emoji");
+const hljs = require("highlight.js"); // https://highlightjs.org
 
 const fs = require("fs");
-const MD = new MarkdownIt({ html: true, langPrefix: "language-" });
+const MD = new MarkdownIt({
+  html: true,
+  langPrefix: "language-",
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(str, { language: lang }).value;
+      } catch (__) {}
+    }
+
+    return ""; // use external default escaping
+  },
+});
 MD.use(markdownItAttrs).use(markdownHSections).use(emoji);
 
 const md = fs.readFileSync("../README.md", { encoding: "utf8" });
