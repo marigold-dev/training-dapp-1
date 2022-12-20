f8e6b21698cd1ad9991773dd7cc7bae0baf65259
Benjamin Fuentes <benjamin.fuentes@marigold.dev>
2022-12-20 18:02:54 +0100

    try last time

--- doc/convert.js
+++ doc/convert.js
@@ -2,21 +2,30 @@ const MarkdownIt = require("markdown-it");
 const markdownItAttrs = require("markdown-it-attrs");
 const markdownHSections = require("markdown-it-header-sections");
 const emoji = require("markdown-it-emoji");
-const hljs = require("./highlight.js"); // https://highlightjs.org
+const hljs = require("highlight.js"); // https://highlightjs.org
+
+hljs.registerLanguage(
+  "javascript",
+  require("highlight.js/lib/languages/javascript")
+);
+hljs.registerLanguage(
+  "typescript",
+  require("highlight.js/lib/languages/typescript")
+);
 
 const fs = require("fs");
 const MD = new MarkdownIt({
   html: true,
   langPrefix: "language-",
   highlight: function (str, lang) {
-    console.log("HELLO", lang);
-    if (lang) {
+    console.log("HELLO", lang, hljs.getLanguage(lang));
+    if (lang && hljs.getLanguage(lang)) {
       try {
         return (
           '<pre class="hljs language-' +
           lang +
           '"><code>' +
-          hljs.highlightAuto(str).value +
+          hljs.highlight(str, { language: lang }).value +
           "</code></pre>"
         );
       } catch (error) {
@@ -35,5 +44,5 @@ const md = fs.readFileSync("../README.md", { encoding: "utf8" });
 const body = MD.render(md);
 
 const template = fs.readFileSync("./template.html", { encoding: "utf8" });
-const html = template.replace("{body}", body);
-fs.writeFileSync("./www/index.html", html);
+const newhtml = template.replace("{body}", body);
+fs.writeFileSync("./www/index.html", newhtml);
