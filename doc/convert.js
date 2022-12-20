29ddbdccfec1e7f342a1c9421f932a7f58b7b981
Benjamin Fuentes <benjamin.fuentes@marigold.dev>
2022-12-20 16:38:58 +0100

    emoji

--- doc/convert.js
+++ doc/convert.js
@@ -1,9 +1,12 @@
 const MarkdownIt = require("markdown-it");
 const markdownItAttrs = require("markdown-it-attrs");
 const markdownHSections = require("markdown-it-header-sections");
+const emoji = require("markdown-it-emoji");
+
 const fs = require("fs");
 const MD = new MarkdownIt({ html: true });
-MD.use(markdownItAttrs).use(markdownHSections);
+MD.use(markdownItAttrs).use(markdownHSections).use(emoji);
+
 const md = fs.readFileSync("../README.md", { encoding: "utf8" });
 const body = MD.render(md);
 
