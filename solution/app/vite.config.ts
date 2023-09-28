3a66b842f6f06255f562cd1412404184d36517ba
Benjamin Fuentes <zamrokk@gmail.com>
2023-09-28 14:52:45 +0200

    fixed

--- solution/app/vite.config.ts
+++ solution/app/vite.config.ts
@@ -1,19 +1,40 @@
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    global: {},
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      stream: "stream-browserify",
-      os: "os-browserify/browser",
-      util: "util",
-      process: "process/browser",
-      buffer: "buffer",
+export default ({ command }) => {
+  const isBuild = command === "build";
+
+  return defineConfig({
+    define: {},
+    plugins: [react()],
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
     },
-  },
-});
+    resolve: {
+      alias: {
+        // dedupe @airgap/beacon-sdk
+        // I almost have no idea why it needs `cjs` on dev and `esm` on build, but this is how it works 🤷‍♂️
+        "@airgap/beacon-sdk": path.resolve(
+          path.resolve(),
+          `./node_modules/@airgap/beacon-sdk/dist/${
+            isBuild ? "esm" : "cjs"
+          }/index.js`
+        ),
+        stream: "stream-browserify",
+        os: "os-browserify/browser",
+        util: "util",
+        process: "process/browser",
+        buffer: "buffer",
+        crypto: "crypto-browserify",
+        assert: "assert",
+        http: "stream-http",
+        https: "https-browserify",
+        url: "url",
+        path: "path-browserify",
+      },
+    },
+  });
+};
